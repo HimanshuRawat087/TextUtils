f9876122dc7b1a609ee0d0bdc6e31921b636b214
Himanshu Rawat <himanshurawat2601@gmail.com>
2023-06-12 22:49:30 +0530

    added preview text area for user to analyse the change

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Textform(props) {
+  const [text, setText] = useState("");
+
+  const handleUpClick = () => {
+    let UpperCaseText = text.toUpperCase();
+    setText(UpperCaseText);
+  };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
   return (
-    <div>
+    <>
+      <div className="Container">
         <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          id="ENTER TEXT HERE!"
-          rows="15"
-        ></textarea>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="Enter your Text"
+            onChange={handleOnChange}
+            rows="15"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
       </div>
-      <button className="btn btn-primary">
-        {props.buttonType}
-      </button>
-    </div>
+      <div className="Container my-5">
+        <h1>Your Text Summary</h1>
+        <p>
+          {text.split(" ").length - 1} words and {text.length} characters
+        </p>
+        <p>{0.008 * (text.split(" ").length - 1)} minuets to read</p>
+        <h2>Preview</h2>
+        <p className="border-3">{text}</p>
+      </div>
+    </>
   );
 }
