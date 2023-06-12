5f0ef39db78ccc04bc4de382d523f16c0eec983a
Himanshu Rawat <himanshurawat2601@gmail.com>
2023-06-12 23:19:57 +0530

    added "convert to lowercase" button

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -4,8 +4,12 @@ export default function Textform(props) {
   const [text, setText] = useState("");
 
   const handleUpClick = () => {
-    let UpperCaseText = text.toUpperCase();
-    setText(UpperCaseText);
+    let newtext = text.toUpperCase();
+    setText(newtext);
+  };
+  const handleLoClick = () => {
+    let newtext = text.toLowerCase();
+    setText(newtext);
   };
 
   const handleOnChange = (event) => {
@@ -15,6 +19,7 @@ export default function Textform(props) {
     <>
       <div className="Container">
         <h1>{props.heading}</h1>
+
         <div className="mb-3">
           <textarea
             className="form-control"
@@ -23,16 +28,23 @@ export default function Textform(props) {
             rows="15"
           ></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>
+
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
           Convert to Uppercase
         </button>
+
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>
+          Convert to Lowercase
+        </button>
       </div>
+
       <div className="Container my-5">
         <h1>Your Text Summary</h1>
         <p>
           {text.split(" ").length - 1} words and {text.length} characters
         </p>
         <p>{0.008 * (text.split(" ").length - 1)} minuets to read</p>
+
         <h2>Preview</h2>
         <p className="border-3">{text}</p>
       </div>
