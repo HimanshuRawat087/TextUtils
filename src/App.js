b1272767cb0acdbd825a1d7efa2a81ee88b02f1c
Himanshu Rawat <himanshurawat2601@gmail.com>
2023-06-14 00:17:42 +0530

    added new component to toggle btw light/dark mode

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import Textform from "./Components/Textform";
 
+import ChangeMode from "./Components/ChangeMode";
+
 function App() {
   return (
     <>
       <Navbar title="TextUtils" />
       <div className="container">
-        <Textform heading="Enter Your Text Below" buttonType="Convert to Uppercase"/>
+        <Textform
+          heading="Enter Your Text Below"
+          buttonType="Convert to Uppercase"
+        />
       </div>
+      <ChangeMode />
     </>
   );
 }
