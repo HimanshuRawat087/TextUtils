1ea873375d63f1c3ddae329784408e7ec4301978
Himanshu Rawat <himanshurawat2601@gmail.com>
2023-06-15 16:42:24 +0530

    created and integrated "Alert" component in app.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Navbar from "./Components/Navbar";
 import Textform from "./Components/Textform";
 import ChangeMode from "./Components/ChangeMode";
+import Alert from "./Components/Alert";
 
 function App() {
 
-  const [mode , setMode] = useState('light');
-  const [BtnText,setText] =useState('Dark Mode');
   
-  const toggleMode = () =>{
-    if(mode ==='light'){
-      setMode('dark')
-      document.body.style.background='#0d283d'
-      setText('Light Mode')
+  const [mode, setMode] = useState("light");
+  const [BtnText, setText] = useState("Dark Mode");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    })
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.background = "#0d283d";
+      setText("Light Mode");
+      showAlert("dark mode is enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.background = "white";
+      setText("Dark Mode");
+      showAlert("light mode is enabled", "success");
     }
-    else{
-      setMode('light')
-      document.body.style.background='white'
-      setText('Dark Mode')
-    }
-  }
+  };
 
   return (
     <>
-      <Navbar title="TextUtils" btnText={BtnText} mode={mode} toggleMode={toggleMode}/>
+      <Navbar
+        title="TextUtils"
+        btnText={BtnText}
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <Alert alert={alert}/>
       <div className="container">
         <Textform
           heading="Enter Your Text Below"
